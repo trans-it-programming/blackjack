a8034787c3812000e2aefc7fe467e631352f449c
Satoshi Tanaka <satstnka@gmail.com>
2021-11-19 15:58:54 +0900

    カードクラス修正

--- src/jp/trans_it/game/blackjack/Card.java
+++ src/jp/trans_it/game/blackjack/Card.java
@@ -1,29 +1,55 @@
 package jp.trans_it.game.blackjack;
 
+/**
+ * カード管理クラス
+ */
 public class Card {
 	public enum Suit {
+		/** スペード */
 		SPADE,
+		/** ハート */
 		HEART,
+		/** ダイヤ */		
 		DIAMOND,
+		/** クラブ */
 		CLUB
 	}
 
 	private Suit suit;
 	private int number;
 
+	/**
+	 * コンストラクター
+	 * @param suit マーク
+	 * @param number 数字
+	 */
 	public Card(Suit suit, int number) {
 		this.suit = suit;
 		this.number = number;
 	}
 
+	/**
+	 * カードのマークを取得する。
+	 * @return カードのマーク
+	 */
 	public Suit getSuit() {
 		return suit;
 	}
 
+	/**
+	 * カードの数字を取得する。
+	 * @return カードの数字
+	 */
 	public int getNumber() {
 		return number;
 	}
 
+	/**
+	 * マーク情報の文字列を取得する。
+	 * (例: Suit.SPADE -> スペード)
+	 * @param suit マーク情報
+	 * @return 文字列
+	 */
 	public static String getSuitString(Suit suit) {
 		String string = null;
 
@@ -42,6 +68,12 @@ public class Card {
 		return string;
 	}
 
+	/**
+	 * 数字情報の文字列を取得する。
+	 * (例: 1 -> A, 2 -> 2, 3-> 3, ... , 12 -> Q, 13 -> K)
+	 * @param number 数字
+	 * @return 数字の文字列
+	 */
 	public static String getNumberString(int number) {
 		String string = null;
 
@@ -64,6 +96,10 @@ public class Card {
 		return string;
 	}
 
+	/**
+	 * カードの文字列を取得する。
+	 * (例: [スペードA], [スペード2], ...)
+	 */
 	public String toString() {
 		String suit = getSuitString(this.suit);
 		String number = getNumberString(this.number);
@@ -76,6 +112,10 @@ public class Card {
 		return string;
 	}
 
+	/**
+	 * 52枚の全てのカードを取得する。
+	 * @return 52枚のカードの配列
+	 */
 	public static Card[] getAllCards() {
 		Card[] cards = new Card[52];
 		int index = 0;
